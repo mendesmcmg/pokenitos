30328b5f4e9275e965fd90832eb0f3a4feb4c929
Clara Mendes <claraplanejamento@gmail.com>
2021-09-23 14:19:47 -0300

    :sparkles: Adds searchfield

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { getAllPokemons } from "./api/api";
-import PokemonCard from "./components/PokemonCard";
+import PokemonCard from "./components/PokemonCard/PokemonCard";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
+  const [searchField, setSearchField] = useState("");
 
   useEffect(() => {
-    getAllPokemons()
-      .then((response) => setPokemons(response))
+    getAllPokemons().then((response) => setPokemons(response));
   }, []);
 
-  console.log(pokemons)
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchField.toLowerCase())
+  );
 
   return (
     <div>
       <header>
         <h1>Pokenitos: uma pokedex ribonita</h1>
-        <div>
-          {pokemons.map((pokemon, i) => (
-            <PokemonCard key={i} name={pokemon.name} url={pokemon.url}/>
-          ))}
-        </div>
       </header>
+
+      <input
+        type="search"
+        placeholder="busque seu pokenito aqui"
+        onChange={e => setSearchField(e.target.value)}
+      />
+      <div>
+        {filteredPokemons.map((pokemon, i) => (
+          <PokemonCard key={i} name={pokemon.name} url={pokemon.url} />
+        ))}
+      </div>
     </div>
   );
 }
