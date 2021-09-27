714ff75489e774d3d3881a226d62e1115b963646
Clara Mendes <claraplanejamento@gmail.com>
2021-09-27 15:27:09 -0300

    :recycle: Removes unnecessary test code

--- src/utils/__test__/idFinder.test.js
+++ src/utils/__test__/idFinder.test.js
@@ -1,30 +1,28 @@
 import idFinder from "../idFinder";
 
-describe("getsIdFromUrl", () => {
-  test("should get Id with 1 digit", () => {
-    expect(
-      idFinder(
-        "https://pokeapi.co/api/v2/pokemon/1/",
-        "https://pokeapi.co/api/v2/pokemon/"
-      )
-    ).toBe("1");
-  });
+test("should get Id with 1 digit", () => {
+  expect(
+    idFinder(
+      "https://pokeapi.co/api/v2/pokemon/1/",
+      "https://pokeapi.co/api/v2/pokemon/"
+    )
+  ).toBe("1");
+});
 
-  test("should get Id with 2 digits", () => {
-    expect(
-      idFinder(
-        "https://pokeapi.co/api/v2/pokemon/10/",
-        "https://pokeapi.co/api/v2/pokemon/"
-      )
-    ).toBe("10");
-  });
+test("should get Id with 2 digits", () => {
+  expect(
+    idFinder(
+      "https://pokeapi.co/api/v2/pokemon/10/",
+      "https://pokeapi.co/api/v2/pokemon/"
+    )
+  ).toBe("10");
+});
 
-  test("should get Id with 3 digits", () => {
-    expect(
-      idFinder(
-        "https://pokeapi.co/api/v2/pokemon/100/",
-        "https://pokeapi.co/api/v2/pokemon/"
-      )
-    ).toBe("100");
-  });
-  })
+test("should get Id with 3 digits", () => {
+  expect(
+    idFinder(
+      "https://pokeapi.co/api/v2/pokemon/100/",
+      "https://pokeapi.co/api/v2/pokemon/"
+    )
+  ).toBe("100");
+});
