7c23b38bea991b8e9bd0b290b61cd6056d8dd772
Clara Mendes <claraplanejamento@gmail.com>
2021-09-27 20:11:12 -0300

    :white_check_mark: Adds details card test

--- src/components/__test__/DetailsCard.test.js
+++ src/components/__test__/DetailsCard.test.js
@@ -1,11 +1,19 @@
 import { mount } from "enzyme";
 import DetailsCard from "../DetailsCard/DetailsCard";
-import details from '../../api/__mocks__/details.json'
+import types from "../../api/__mocks__/types.json";
+import details from "../../api/__mocks__/details.json";
+import typesResponseParser from "../../utils/typesResponseParser";
 
-test("should render evolutions data with type", ()=>{
-  const 
-  const wrapper = mount(<DetilsCard details={details} type={type} />)
+test("should render evolutions data with type", () => {
+  const type = typesResponseParser(types);
+  const wrapper = mount(<DetailsCard details={details} type={type} />);
 
-  expect(wrapper.find("p").first().text()).toBe("25")
-  expect(wrapper.find("p").at(1).text()).toBe("pikachu")
-})
+  expect(wrapper.find("p").first().text()).toBe("Type: electric");
+});
+
+test("should render evolutions data with habitat", () => {
+  const type = typesResponseParser(types);
+  const wrapper = mount(<DetailsCard details={details} type={type} />);
+
+  expect(wrapper.find("p").at(1).text()).toBe("Habitat: forest");
+});
